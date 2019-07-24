8408d4f9dcc0f18a49bb8300bd8006660ad7ec51
Abhinav Reddy M <abhinav.reddy@codenicely.in>
2019-07-25 03:03:25 +0530

    settings adjusted

--- django_project_template/settings.py
+++ django_project_template/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'django_project_template.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR + '/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
